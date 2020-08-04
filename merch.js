c360c2dbd375e854aa27133e382a5a31d340f57f
Léo Pichat <leo.pichat1257@gmail.com>
2020-08-04 19:39:31 +0200

    first commit

---
+++ merch.js
@@ -0,0 +1,37 @@
+const merch = [
+  {
+    name : "T-shirt La Frappe Post Tenebras Lux",
+    price : "1000.-",
+    image : "img_shop/tshirt.jpg",
+    quote : "\" I like smoking weed \" - quentin"
+  },
+  {
+    name : "Pull La Frappe Post Tenebras Lux",
+    price : "10000000.-",
+    image : "img_shop/tshirt.jpg",
+    quote : "\" I like smoking weed \" - quentin"
+  },
+  {
+    name : "Banane la frappe",
+    price : "10000000000.-",
+    image : "img_shop/tshirt.jpg",
+    quote : "\" I like smoking weed \" - quentin"
+  }
+];
+
+//Modèle d'un article
+/*<div class="shop_item">
+    <img class="img_item" src="img_shop/tshirt.jpg"/>
+    <div class="item_infos">
+    <div class="item_title">T-Shirt</div>
+    <div class="item_price"><b>19.90.-</b></div>
+<div class="item_quote"><i>
+    " Youpi " - Guimove
+    </i></div>
+</div>
+<div class="control_number" id="control_panier_0">Ajouté au panier : 0</div>
+<div class="shop_controls">
+    <a href="#"><img class="add_article" src="img_shop/add.png" onclick="addArticle(0)"/></a><br/>
+    <a href="#"><img class="delete_article" src="img_shop/delete.png" onclick="removeArticle(0)"/></a>
+    </div>
+    </div>*/
