f681bb9d21bd266e56087f824b36a4fddf59c598
Léo Pichat <leo.pichat1257@gmail.com>
2020-11-25 16:57:03 +0100

    More rework

--- lafrappewebsite/merch.js
+++ lafrappewebsite/merch.js
@@ -1,42 +1,37 @@
 const merch = [
   {
-    owner: 0,
     name : "T-shirt POST TENEBRAS LUX",
     price : "32",
-    image : "img_shop/kentus-post-1.jpg",
+    images : ["img_shop/kentus-post-1.jpg", "img_shop/kentus-post-2.jpg", "img_shop/kentus-post-3.jpg", "img_shop/kentus-post-4.jpg", "img_shop/kentus-post-5.jpg", "img_shop/kentus-post-6.jpg"],
     quote : "Le t-shirt avec le logo du premier film de Kentus, un design aussi maléfique et bordelique que le film, représentant un «Frappe jack » composé des rues et immeubles emblématique de Genève et des ses milles périls …",
     colors: ["Bleu", "Noir"]
   },
   {
-    owner: 0,
     name : "Affiche POST TENEBRAS LUX",
     price : "10",
-    image : "img_shop/kentus-post-2.jpg", 
+    images : ["img_shop/kentus-affiche.png"], 
     quote : "Affiche du premier film de Kentus : LA FRAPPE ACTE 1 POST TENEBRAS LUX, un film de parkour complétement dégénéré et décalé disponible <a href=\"https://www.youtube.com/watch?v=qtiOBC_3Xtc\">sur youtube</a>.",
-    colors: ["Bleu", "Noir"]
+    colors: []
   },
   {
-    owner: 1,
     name : "T-shirt La Frappe 2021",
     price : "32",
-    image : "img_shop/frappe-ts-1.jpg",
+    images : ["img_shop/frappe-ts-1.jpg", "img_shop/frappe-ts-2.jpg", "img_shop/frappe-ts-3.jpg", "img_shop/frappe-ts-4.jpg"],
     quote : "Le T-shirt la Frappe de la collection 2021, une coupe ajustée et travaillé pour être la plus ample et agréable possible avec un logo éléctrique au couleurs rétro et agressives rappelant les influences synthés des débuts de la team.",
     colors: ["Bleu", "Noir"]
   },
   {
-    owner: 1,
     name : "T-shirt La Frappe Wall Breaker",
     price : "30",
-    image : "img_shop/frappe-wb-1.jpg",
+    images : ["img_shop/frappe-wb-1.jpg", "img_shop/frappe-wb-2.jpg", "img_shop/frappe-wb-3.jpg", "img_shop/frappe-wb-4.jpg"],
     quote : "Le T-shirt de l’édition 2020 avec son logo qui cassent des baraques et sa coupe légère et décontracté, parfait pour les entrainements sous grosses chaleurs.",
     colors: ["Noir"]
   },
   {
-    owner: 1,
     name : "Patch La Frappe society",
     price : "6",
-    image : "img_shop/frappe-ts-2.jpg",
+    images : ["img_shop/frappe-patch.jpg"], 
     quote : "Parfait pour couvrir avec classe les trous des trainings déchirés par des sessions de parkour trop violentes ou styliser ces vieux habilles.",
-    colors: ["Noir"]
+    colors: []
   }
 ];
--- merch.js
+++ merch.js
@@ -1,27 +1,39 @@
 module.exports = [
-    {
-      name : "T-shirt La Frappe Post Tenebras Lux",
-      price : "1000",
-      image : "img_shop/tshirt.jpg",
-      quote : "\" I like smoking weed \" - quentin"
-    },
-    {
-      name : "Pantacourt La Frappe",
-      price : "19.90",
-      image : "img_shop/tshirt.jpg",
-      quote : "\" I like smoking weed \" - quentin"
-    },
-    {
-      name : "Banane la frappe",
-      price : "35",
-      image : "img_shop/tshirt.jpg",
-      quote : "\" I like smoking weed \" - quentin"
-    },
-    {
-      name : "Pipe à crack la frappe",
-      price : "12.50",
-      image : "img_shop/tshirt.jpg",
-      quote : "\" I like smoking weed \" - quentin"
-    }
-  ];
+  {
+    name : "T-shirt POST TENEBRAS LUX",
+    price : "32",
+    images : ["img_shop/kentus-post-1.jpg", "img_shop/kentus-post-2.jpg", "img_shop/kentus-post-3.jpg", "img_shop/kentus-post-4.jpg", "img_shop/kentus-post-5.jpg", "img_shop/kentus-post-6.jpg"],
+    quote : "Le t-shirt avec le logo du premier film de Kentus, un design aussi maléfique et bordelique que le film, représentant un «Frappe jack » composé des rues et immeubles emblématique de Genève et des ses milles périls …",
+    colors: ["Bleu", "Noir"]
+  },
+  {
+    name : "Affiche POST TENEBRAS LUX",
+    price : "10",
+    images : ["img_shop/tshirt.jpg"], 
+    quote : "Affiche du premier film de Kentus : LA FRAPPE ACTE 1 POST TENEBRAS LUX, un film de parkour complétement dégénéré et décalé disponible <a href=\"https://www.youtube.com/watch?v=qtiOBC_3Xtc\">sur youtube</a>.",
+    colors: []
+  },
+  {
+    name : "T-shirt La Frappe 2021",
+    price : "32",
+    images : ["img_shop/frappe-ts-1.jpg", "img_shop/frappe-ts-2.jpg", "img_shop/frappe-ts-3.jpg", "img_shop/frappe-ts-4.jpg"],
+    quote : "Le T-shirt la Frappe de la collection 2021, une coupe ajustée et travaillé pour être la plus ample et agréable possible avec un logo éléctrique au couleurs rétro et agressives rappelant les influences synthés des débuts de la team.",
+    colors: ["Bleu", "Noir"]
+  },
+  {
+    name : "T-shirt La Frappe Wall Breaker",
+    price : "30",
+    images : ["img_shop/frappe-wb-1.jpg", "img_shop/frappe-wb-2.jpg", "img_shop/frappe-wb-3.jpg", "img_shop/frappe-wb-4.jpg"],
+    quote : "Le T-shirt de l’édition 2020 avec son logo qui cassent des baraques et sa coupe légère et décontracté, parfait pour les entrainements sous grosses chaleurs.",
+    colors: ["Noir"]
+  },
+  {
+    name : "Patch La Frappe society",
+    price : "6",
+    images : ["img_shop/tshirt.jpg"], 
+    quote : "Parfait pour couvrir avec classe les trous des trainings déchirés par des sessions de parkour trop violentes ou styliser ces vieux habilles.",
+    colors: []
+  }
+];
+
   
